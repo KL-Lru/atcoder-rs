d2dcd6b70b027fb712d2e5ce9006f6f7379c059b
KL-Lru <26233827+KL-Lru@users.noreply.github.com>
2025-05-23 22:41:59 +0900

    solve ABC 392 A

---
+++ abc_392/docs/a.md
@@ -0,0 +1,3 @@
+## ABC 392 A - Shuffled Equation
+
+refs: <https://atcoder.jp/contests/abc392/tasks/abc392_a>
---
+++ abc_392/src/bin/a.rs
@@ -0,0 +1,15 @@
+use proconio::input;
+
+fn main() {
+    input! {
+        mut a: [usize; 3]
+    };
+
+    a.sort();
+
+    if a[0] * a[1] == a[2] {
+        println!("Yes")
+    } else {
+        println!("No")
+    }
+}
