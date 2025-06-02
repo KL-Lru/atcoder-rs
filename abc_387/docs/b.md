f8eb85ff40e60da20d3dc1a808acf16fbff47dc8
KL-Lru <26233827+KL-Lru@users.noreply.github.com>
2025-06-03 02:07:19 +0900

    solve ABC 387 B

---
+++ abc_387/docs/b.md
@@ -0,0 +1,3 @@
+## ABC 387 B - 9x9 Sum
+
+refs: <https://atcoder.jp/contests/abc387/tasks/abc387_b>
---
+++ abc_387/src/bin/b.rs
@@ -0,0 +1,19 @@
+use proconio::input;
+
+fn main() {
+    input! {
+        x: usize,
+    }
+
+    let mut ans = 0;
+
+    for i in 1..=9 {
+        for j in 1..=9 {
+            if i * j != x {
+                ans += i * j;
+            }
+        }
+    }
+
+    println!("{ans}");
+}
