e1c698cdb4196dba5f5e3e45121fb08bb13c7d86
KL-Lru <26233827+KL-Lru@users.noreply.github.com>
2025-06-07 01:29:41 +0900

    solve ABC 192 A

---
+++ abc_192/docs/a.md
@@ -0,0 +1,3 @@
+## ABC 192 A - Star
+
+refs: <https://atcoder.jp/contests/abc192/tasks/abc192_a>
---
+++ abc_192/src/bin/a.rs
@@ -0,0 +1,9 @@
+use proconio::input;
+
+fn main() {
+    input! {
+        x: i64,
+    };
+
+    println!("{}", 100 - (x % 100));
+}
