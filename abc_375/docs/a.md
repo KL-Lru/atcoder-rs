c5ce96c2e919e50a2d50fb55b940143dc0852c2e
KL-Lru <26233827+KL-Lru@users.noreply.github.com>
2025-06-25 21:52:34 +0900

    solve ABC 375 A

---
+++ abc_375/docs/a.md
@@ -0,0 +1,3 @@
+## ABC 375 A - Seats
+
+refs: <https://atcoder.jp/contests/abc375/tasks/abc375_a>
---
+++ abc_375/src/bin/a.rs
@@ -0,0 +1,22 @@
+use proconio::{input, marker::Chars};
+
+fn main() {
+    input! {
+        n: usize,
+        s: Chars,
+    }
+
+    let mut ans = 0;
+    if s.len() <= 2 {
+        println!("0");
+        return;
+    }
+
+    for i in 0..(n - 2) {
+        if (s[i], s[i + 1], s[i + 2]) == ('#', '.', '#') {
+            ans += 1;
+        }
+    }
+
+    println!("{ans}");
+}
