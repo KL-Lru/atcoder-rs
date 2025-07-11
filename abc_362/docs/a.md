38a433b60b9556f38adf8a186621744d5eabfdeb
KL-Lru <26233827+KL-Lru@users.noreply.github.com>
2025-07-11 22:48:12 +0900

    solve ABC 362 A

---
+++ abc_362/docs/a.md
@@ -0,0 +1,3 @@
+## ABC 362 A - Buy a Pen
+
+refs: <https://atcoder.jp/contests/abc362/tasks/abc362_a>
---
+++ abc_362/src/bin/a.rs
@@ -0,0 +1,17 @@
+use proconio::input;
+
+fn main() {
+    input! {
+        r: usize, g: usize, b: usize,
+        c: String,
+    }
+
+    let ans = match c.as_str() {
+        "Red" => g.min(b),
+        "Green" => r.min(b),
+        "Blue" => r.min(g),
+        _ => unreachable!(),
+    };
+
+    println!("{ans}");
+}
