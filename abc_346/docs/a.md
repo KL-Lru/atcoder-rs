6f37c64af1e0405cee3ebede681a3c21559f164a
KL-Lru <26233827+KL-Lru@users.noreply.github.com>
2025-08-04 23:52:02 +0900

    solve ABC 346 A

---
+++ abc_346/docs/a.md
@@ -0,0 +1,3 @@
+## ABC 346 A - Adjacent Product
+
+refs: <https://atcoder.jp/contests/abc346/tasks/abc346_a>
---
+++ abc_346/src/bin/a.rs
@@ -0,0 +1,22 @@
+use proconio::input;
+
+fn main() {
+    input! {
+        n: usize,
+        a: [usize; n],
+    }
+
+    let mut ans = vec![];
+
+    for i in 1..n {
+        ans.push(a[i] * a[i - 1]);
+    }
+
+    println!(
+        "{}",
+        ans.iter()
+            .map(|x| x.to_string())
+            .collect::<Vec<_>>()
+            .join(" ")
+    )
+}
